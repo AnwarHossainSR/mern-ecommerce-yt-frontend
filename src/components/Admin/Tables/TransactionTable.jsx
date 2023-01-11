dfe6552d0a3fa55bbb69d25e84e71d2545b20bd3
Anwar Hossain <anwar.sarker@bjitgroup.com>
2023-01-11 12:27:20 +0600

    fixd issue

--- src/components/Admin/Tables/TransactionTable.jsx
+++ src/components/Admin/Tables/TransactionTable.jsx
@@ -4,7 +4,6 @@ import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
 import TableContainer from '@mui/material/TableContainer';
-import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 
 function createData(name, date, amount) {

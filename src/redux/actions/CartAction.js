3efbdf00d78f6d6b6768d06d8ba551b286723737
Restyled.io <commits@restyled.io>
2023-01-13 06:47:42 +0000

    Restyled by prettier

--- src/redux/actions/CartAction.js
+++ src/redux/actions/CartAction.js
@@ -1,4 +1,4 @@
-import {notify} from '../../utils/helper';
+import { notify } from '../../utils/helper';
 import {
   cartFailure,
   cartPending,
@@ -8,18 +8,18 @@ import {
   shippingSuccess,
 } from '../reducers/CartSlice';
 
-export const storeProductInCart = (cartProducts, product) =>
-    async (dispatch) => {
-  dispatch(cartPending());
-  try {
-    const carts = new Array(...cartProducts);
-    carts.push(product);
-    dispatch(cartSuccess(carts));
-    notify('Product Added to Cart', 'success');
-  } catch (error) {
-    dispatch(cartFailure(error.message));
-  }
-};
+export const storeProductInCart =
+  (cartProducts, product) => async (dispatch) => {
+    dispatch(cartPending());
+    try {
+      const carts = new Array(...cartProducts);
+      carts.push(product);
+      dispatch(cartSuccess(carts));
+      notify('Product Added to Cart', 'success');
+    } catch (error) {
+      dispatch(cartFailure(error.message));
+    }
+  };
 
 export const clearCartsAction = () => async (dispatch) => {
   dispatch(cartPending());
@@ -31,19 +31,19 @@ export const clearCartsAction = () => async (dispatch) => {
   }
 };
 
-export const removeSpecificCartFromCartAction = (cartProducts, cart) =>
-    async (dispatch) => {
-  try {
-    dispatch(cartPending());
-    const carts = new Array(...cartProducts);
-    const updatedCarts = carts.filter((item) => item._id !== cart._id);
-    dispatch(cartSuccess(updatedCarts));
-    notify('Product removed from cart', 'success');
-  } catch (error) {
-    dispatch(cartFailure(error.message));
-    notify('Something went wrong', 'error');
-  }
-};
+export const removeSpecificCartFromCartAction =
+  (cartProducts, cart) => async (dispatch) => {
+    try {
+      dispatch(cartPending());
+      const carts = new Array(...cartProducts);
+      const updatedCarts = carts.filter((item) => item._id !== cart._id);
+      dispatch(cartSuccess(updatedCarts));
+      notify('Product removed from cart', 'success');
+    } catch (error) {
+      dispatch(cartFailure(error.message));
+      notify('Something went wrong', 'error');
+    }
+  };
 
 export const storShippingInfoAction = (shippingInfo) => async (dispatch) => {
   console.log(shippingInfo);

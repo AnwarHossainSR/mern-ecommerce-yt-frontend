fc957260b22a5fb664db8ee576d8273b4c25b167
Restyled.io <commits@restyled.io>
2023-01-15 16:51:21 +0000

    Restyled by prettier

--- src/API/CallAPI.js
+++ src/API/CallAPI.js
@@ -6,7 +6,7 @@ axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 axios.defaults.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`;
 axios.defaults.headers.post['Content-Type'] = 'application/json;charset=utf-8';
 axios.defaults.headers.common['Access-Control-Allow-Origin'] =
-    'GET,PUT,POST,DELETE,PATCH,OPTIONS';
+  'GET,PUT,POST,DELETE,PATCH,OPTIONS';
 
 export const getApi = async (url) => {
   const res = await axios.get(url);
